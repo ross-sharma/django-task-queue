e22fe35cb7f7a561696f647bc19f9f93419d9316
Ross Sharma <ross.sharma86@gmail.com>
2021-03-30 11:49:50 -0700

    Fix app config not being used

---
+++ __init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'django_task_queue.apps.DjangoTaskQueueConfig'
--- apps.py
+++ apps.py
@@ -7,6 +7,6 @@ class DjangoTaskQueueConfig(AppConfig):
     def ready(self):
         from django.conf import settings
         from . import app_settings
-        project_settings = settings.get_setting('DJANGO_TASK_QUEUE', {})
+        project_settings = getattr(settings, 'DJANGO_TASK_QUEUE', {})
         for key, val in project_settings.items():
             setattr(app_settings, key, val)
