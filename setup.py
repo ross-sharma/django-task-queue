f22cd63d08dc6871a841c66c53fa099c5cca7e6f
Ross Sharma <ross.sharma86@gmail.com>
2021-04-19 00:46:31 -0700

    Commit the entire dummy project

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="django-task-queue",
+    version="1.0.4",
+    description="Simple task queue for Python Django framework",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ross-sharma/django_task_queue",
+    author="Ross Sharma",
+    author_email="ross@ross-sharma.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(exclude=["dummy_project"]),
+    include_package_data=True,
+    install_requires=["django"],
+    entry_points={
+        "console_scripts": []
+    },
+)
